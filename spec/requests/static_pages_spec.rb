b62d3316f511aaf0ee531c4d9da4e19c0eb26dcd
Sahil Muthoo <sahil.muthoo@gmail.com>
2012-10-15 21:38:56 +0530

    Finish chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,28 +1,36 @@
 require 'spec_helper'
 
-describe "StaticPages" do
+describe "Static pages" do
   subject { page }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_selector('title', text: title) }
+  end
   describe "Home Page" do
     before { visit root_path }
-    it { should have_selector('h1', text: "Sample App") }
-    it { should have_selector('title', text: full_title('')) }
+    let(:heading) { 'Sample App' }
+    let(:title)   { full_title('') }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help Page" do
     before { visit help_path }
-    it { should have_selector('h1', text: 'Help') }
-    it { page.should have_selector('title', text: full_title('Help')) }
+    let(:heading) { 'Help' }
+    let(:title)   { full_title('Help') }
+    it_should_behave_like "all static pages"
   end
 
   describe "About Page" do
     before { visit about_path }
-    it { page.should have_selector('h1', text: 'About Us') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    let(:heading) { 'About' }
+    let(:title)   { full_title('About') }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact Page" do
     before { visit contact_path }
-    it { should have_selector('h1', text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    let(:heading) { 'Contact' }
+    let(:title)   { full_title('Contact') }
+    it_should_behave_like "all static pages"
   end
 end
